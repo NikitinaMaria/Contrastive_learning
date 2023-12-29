9e7aad1730839a8c66a903ccca0a64df2d816d40
NikitinaMaria <71255897+NikitinaMaria@users.noreply.github.com>
2023-12-29 19:43:50 +0300

    Update loss.py

--- Code/loss.py
+++ Code/loss.py
@@ -5,11 +5,10 @@ import torch
 import torch.nn as nn
 
 
-def get_negative_mask(batch_size):
+def get_negative_mask(int: batch_size) -> torch.Tensor:
     """
-    For both images, takes the positions corresponding to itself and
-    to its augmentation. Resulting mask is a matrix (2bs, 2bs) with 
-    the main diagonal and 2 positive parallel sub-diagonals zeroed-out
+    Шаблон для маски с нулями на главной диагонали и на двух
+    параллельных ей
     """
     negative_mask = torch.ones((batch_size, 2 * batch_size), dtype=bool)
     for i in range(batch_size):
@@ -22,16 +21,16 @@ def get_negative_mask(batch_size):
 
 def get_target_mask(target: torch.Tensor) -> torch.Tensor:
     """
-    Generate bool matrix for equal targets
+    Маска для элементов одного класса
     """
-    target_ = torch.cat([target, target], dim=0).view(-1, 1)  # (2bs,)
-    mask = (target_ == target_.t().contiguous()) & (target_ != -1)  # (2bs, 2bs)
+    target_ = torch.cat([target, target], dim=0).view(-1, 1)
+    mask = (target_ == target_.t().contiguous()) & (target_ != -1)
     return mask
 
 
 def replace_pos_diagonals(full_matrix, pos_m):
     """
-    Replace positive sub-diagonals in full_matrix shaped (2bs, 2bs) by pos_m vector
+    Удаление положительный саб-диагоналей
     """
     batch_size = full_matrix.shape[0] // 2
 
@@ -51,28 +50,20 @@ class ContrastiveLossBase(nn.Module):
 
     def forward(self, out_1, out_2, target):
         batch_size = out_1.shape[0]
+        
+        out = torch.cat([out_1, out_2], dim=0)
+        # Скалярное произведение
+        full = torch.exp(torch.mm(out, out.t().contiguous()) / self.temperature)
 
-        ### neg score ###
-        out = torch.cat([out_1, out_2], dim=0)  # (2bs, d)
-        # scalar product of all pairs
-        full = torch.exp(torch.mm(out, out.t().contiguous()) / self.temperature)  # (2bs, 2bs)
-        # drop false-negaive, if needed
-        if self.drop_fn:
-            mask = get_target_mask(target)
-            if self.cuda:
-                mask = mask.cuda()
-            full = full.masked_fill(mask, 0.0)
-
-        # keep negative pairs
-        mask = get_negative_mask(batch_size)  # (2bs, 2bs)
+        # Негативные пары
+        mask = get_negative_mask(batch_size)
         if self.cuda:
             mask = mask.cuda()
-        neg = full.masked_select(mask).view(2 * batch_size, -1)  # (2bs, 2bs - 2)
-
-        ### pos score ####
-        # scalar product of 2 augmentations of 1 image
-        pos = torch.exp(torch.sum(out_1 * out_2, dim=-1) / self.temperature)  # (bs,)
-        pos = torch.cat([pos, pos], dim=0)  # (2bs,)
+        neg = full.masked_select(mask).view(2 * batch_size, -1)
+        
+        # Скалярное произведение двух позитивных элементов
+        pos = torch.exp(torch.sum(out_1 * out_2, dim=-1) / self.temperature)
+        pos = torch.cat([pos, pos], dim=0)
 
         return pos, neg
 
@@ -83,37 +74,57 @@ class ContrastiveLossBase(nn.Module):
 class ContrastiveLoss(ContrastiveLossBase):
     def forward(self, out_1, out_2, out_m, target):
         pos, neg = super().forward(out_1, out_2, target)
-        Ng = neg.sum(dim=-1)  # (2bs)
-        loss = (-torch.log(pos / (pos + Ng))).mean() # contrastive loss
+        Ng = neg.sum(dim=-1)
+        loss = (-torch.log(pos / (pos + Ng))).mean()
         return loss
 
-
-class DebiasedNegLoss(ContrastiveLossBase):
+class DebiasedPosLoss(nn.Module):
     def __init__(self, temperature, cuda, drop_fn, tau_plus):
-        super().__init__(temperature, cuda, drop_fn)
-        self.tau_plus = tau_plus
+        super().__init__()
+        self.temperature: float = temperature
+        self.cuda: bool = cuda
+        self.drop_fn: bool = drop_fn
+        self.tau_plus: float = tau_plus
 
     def forward(self, out_1, out_2, out_m, target):
-        pos, neg = super().forward(out_1, out_2, target)
         batch_size = out_1.shape[0]
+        N = batch_size * 2 - 2
 
+        out = torch.cat([out_1, out_2], dim=0)
+        full = torch.exp(torch.mm(out, out.t().contiguous()) / self.temperature)
+
+        # Позитивные классы
+        pos = torch.exp(torch.sum(out_1 * out_2, dim=-1) / self.temperature)
+        pos = torch.cat([pos, pos], dim=0)
         pos_m = [pos]
-        for vec in out_m:
-            pos_1 = torch.exp(torch.sum(out_1 * vec, dim=-1) / self.temperature)  # (bs,)
-            pos_2 = torch.exp(torch.sum(out_2 * vec, dim=-1) / self.temperature) # (bs,)
-            pos_new = torch.cat([pos_1, pos_2], dim=0)  # (2bs,)
+        for vec in out_m: 
+            pos_1 = torch.exp(torch.sum(out_1 * vec, dim=-1) / self.temperature)
+            pos_2 = torch.exp(torch.sum(out_2 * vec, dim=-1) / self.temperature)
+            pos_new = torch.cat([pos_1, pos_2], dim=0)
             pos_m.append(pos_new)
         pos_m = torch.stack(pos_m, dim=0).mean(dim=0)
 
-        N = batch_size * 2 - 2
-        Ng = (-self.tau_plus * N * pos_m + neg.sum(dim=-1)) / (1 - self.tau_plus)
-        # constrain (optional)
-        Ng = torch.clamp(Ng, min=N * np.e ** (-1 / self.temperature))
-        loss = (-torch.log(pos / (pos + Ng))).mean() # contrastive loss
+        full = replace_pos_diagonals(full, pos_m)
+        p_estimate = full.mean(dim=-1)
+
+        # Негативные классы
+        neg_mask = get_negative_mask(batch_size)
+        if self.cuda:
+            neg_mask = neg_mask.cuda()
+        neg = full.masked_select(neg_mask).view(2 * batch_size, -1)
+
+        # Вычисление лосс-функции
+        tau_minus = 1 - self.tau_plus
+        g = neg.mean(dim=-1)  # (2bs,)
+        numerator = p_estimate - tau_minus * g
+        denominator = p_estimate + (N * self.tau_plus - tau_minus) * g
+        loss = (-torch.log(numerator / denominator)).mean()
+
         return loss
 
     def extra_repr(self):
-        return "Tau plus: {}".format(self.tau_plus)
+        return "Temperature: {}\nCuda: {}\nDrop FN{}\nTau plus: {}".format(
+            self.temperature, self.cuda, self.drop_fn, self.tau_plus)
 
 def get_loss(
     name: str,
@@ -126,10 +137,6 @@ def get_loss(
 
     if name == "Contrastive":
         return ContrastiveLoss(temperature, cuda, drop_fn)
-    if name == "DebiasedNeg":
-        return DebiasedNegLoss(temperature, cuda, drop_fn, tau_plus)
     if name == "DebiasedPos":
         return DebiasedPosLoss(temperature, cuda, drop_fn, tau_plus)
-    if name == "Combination":
-        return CombinationLoss(temperature, cuda, drop_fn, tau_plus, alpha)
-    raise Exception("Unknown loss {}".format(name))
+    raise Exception("Неправильный лосс {}".format(name))
